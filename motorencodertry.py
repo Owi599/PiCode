a2005c168cf26e12e298457171e982185ed7550b
Owi599 <alsabbaghowis@gmail.com>
2025-11-03 09:28:31 +0100

    vaalues

--- motorencodertry.py
+++ motorencodertry.py
@@ -3,16 +3,18 @@ from rotaryencoder import ReadRotaryEncoder
 import time
 motor_encoder_A = 20
 motor_encoder_B = 21
-encoder_a = 7
-encoder_b = 8
+encoder_a = 16
+encoder_b = 12
 cpr=1250
 microstep = 4
-ENCODDER = ReadRotaryEncoder(motor_encoder_A, motor_encoder_B, max_steps=625, wrap=True)
+ENCODDER = ReadRotaryEncoder(motor_encoder_A, motor_encoder_B, max_steps = 625, wrap=True)
+ENCODDER_2 = ReadRotaryEncoder(encoder_a, encoder_b, max_steps=625, wrap=True)
 currentTime = 0
-ENCODDER.steps = 625
-
+ENCODDER.steps =625
+ENCODDER_2.steps =0
 while True:
     position = ENCODDER.read_position(cpr)
+    position_2 = ENCODDER_2.read_position(cpr)
     # velocity, currentTime, currentSteps = ENCODDER.read_velocity(cpr, currentTime, currentSteps)
-    print(f"Position: {(position*100):.2f} cm")
-    time.sleep(0.02)  # Adjust the sleep time as needed
+    print(f"Position: {(position):.2f}, position2 : {(position_2):.2f} ")
+    time.sleep(0.05)
